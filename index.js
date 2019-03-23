120cc6ff5fdba4fbb001fb800ec4bccc10ca1fd6
OrbisCorporation <mrjanzx@gmail.com>
2019-03-23 09:24:49 -0500

    Terminado tutorial

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-var fs = require('fs');
+// const http = require('http');
+//
+// const colors = require('colors');
+// const serverCallback = function(req, res){
+//   res.writeHead(200,{'content-type' : 'text/html'});
+//   res.write('<h1>Hola mundo</h1>');
+//   res.end();
+// }
+//
+// const hServer = http.createServer(serverCallback);
+//
+// hServer.listen(3000, function(){
+//   console.log('Servidor en puerto 3000'.yellow);
+// });
 
-fs.writeFile('./text.txt', 'Primera linea',function(err){
-  if (err) {
-    console.log(err);
-  }
-  console.log('Archivo creado con exito.');
+const express = require('express');
+const colors = require('colors');
+
+const hServer = express();
+
+hServer.get('/',(req, res) => {
+  res.send('<h1>Hola mundo express</h1>');
+  res.end();
 });
 
-
-fs.readFile('./text.txt',function(err, data){
-  if (err) {
-    console.log(err);
-  }
-  console.log(data);
-  console.log(data.toString());
+hServer.listen(3000, () => {
+  console.log('server port 3000'.red);
 });
